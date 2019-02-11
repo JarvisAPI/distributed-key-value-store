a4cfab2157795adc4d431ed6fa7b71802d5e705c
Xingyu Tao <taoxingyu@gmail.com>
2019-02-11 08:55:41 -0800

    implemented direct route class with hardcoded ranges, suggest making an abstract hash node range object to manage the ranges in the future

--- src/com/g8A/CPEN431/A4/server/distribution/DirectRoute.java
+++ src/com/g8A/CPEN431/A4/server/distribution/DirectRoute.java
@@ -6,12 +6,33 @@ package com.g8A.CPEN431.A4.server.distribution;
  * Amount of state (number of host/port stored): O(N)
  */
 public class DirectRoute implements RouteStrategy {
-    // TODO: Hard code node values here...
+	// For now the host name and ports of the nodes are hard coded
+	private static final int DEFAULT_PORT = 5000;
+    private static final AddressHolder node1 = new AddressHolder("pl1.eng.monash.edu.au", DEFAULT_PORT);
+    private static final AddressHolder node2 = new AddressHolder("planetlab-4.eecs.cwru.edu", DEFAULT_PORT);
+    private static final AddressHolder node3 = new AddressHolder("planetlab1.cs.ubc.ca", DEFAULT_PORT);
+    private static final AddressHolder node4 = new AddressHolder("planetlab2.cs.ubc.ca", DEFAULT_PORT);
     
     @Override
+    /**
+     * Get the addess and port of the node to route to, given the nodeId
+     * as the id of the final destination node.
+     * @param nodeId the id of the final destination node
+     * @return hostname and port of node to route to, null if value is 
+     */
     public AddressHolder getRoute(int nodeId) {
-        // TODO: implement
-        return null;
+    	if(nodeId >= 0 && nodeId < 64) {
+        	return node1;
+        }else if(nodeId >= 64 && nodeId < 128) {
+        	return node2;
+        }else if(nodeId >= 128 && nodeId < 192) {
+        	return node3;
+        }else if(nodeId >= 192 && nodeId < 256) {
+        	return node4;
+        }else {
+        	return null;
+        }
     }
-
+    
+    
 }
