2f17d9379e4155105ec91831a2acdcc331222ecd
William Ou <ouhaoqian@hotmail.com>
2019-02-14 12:36:01 -0800

    - More change to KVClient interface

--- src/com/g8A/CPEN431/A4/client/KVClient.java
+++ src/com/g8A/CPEN431/A4/client/KVClient.java
@@ -1,15 +1,16 @@
 package com.g8A.CPEN431.A4.client;
 
+import com.g8A.CPEN431.A4.protocol.NetworkMessage;
 import com.g8A.CPEN431.A4.server.distribution.RouteStrategy.AddressHolder;
 
 import ca.NetSysLab.ProtocolBuffers.KeyValueRequest;
+import ca.NetSysLab.ProtocolBuffers.Message;
 
 public interface KVClient {
     /**
      * Send a given key/value request to another host.
-     * @param request the key/value request to send.
-     * @param holder the destination host.
+     * @param msg the network message to send.
      * @throws IllegalStateException if the sending queue is full.
      */
-    public void send(KeyValueRequest.KVRequest request, AddressHolder holder) throws IllegalStateException;
+    public void send(NetworkMessage msg) throws IllegalStateException;
 }
