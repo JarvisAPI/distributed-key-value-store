0589aad3e9181d73baed2f751a49acd2a5b8755d
Xingyu Tao <taoxingyu@gmail.com>
2019-02-15 01:30:41 -0800

    updated DirectRoute to singleton pattern

--- src/com/g8A/CPEN431/A6/server/distribution/DirectRoute.java
+++ src/com/g8A/CPEN431/A6/server/distribution/DirectRoute.java
@@ -1,18 +1,30 @@
 package com.g8A.CPEN431.A6.server.distribution;
 
+import com.g8A.CPEN431.A6.server.KeyValueStore;
+
 /**
  * This strategy for routing directly routes to the destination node.
  * Number of hops: O(1)
  * Amount of state (number of host/port stored): O(N)
+ * Updated to Singleton pattern
  */
 public class DirectRoute implements RouteStrategy {    
 	
 	// For now the host name and ports of the nodes are hard coded
-	private static final int DEFAULT_PORT = 5000;
-    private static final AddressHolder node1 = new AddressHolder("pl1.eng.monash.edu.au", DEFAULT_PORT);
-    private static final AddressHolder node2 = new AddressHolder("planetlab-4.eecs.cwru.edu", DEFAULT_PORT);
-    private static final AddressHolder node3 = new AddressHolder("planetlab1.cs.ubc.ca", DEFAULT_PORT);
-    private static final AddressHolder node4 = new AddressHolder("planetlab2.cs.ubc.ca", DEFAULT_PORT);
+	private final int DEFAULT_PORT = 5000;
+    private final AddressHolder node1; 
+    private final AddressHolder node2;
+    private final AddressHolder node3; 
+    private final AddressHolder node4; 
+    
+    private static DirectRoute mDirectRoute;
+    
+    private DirectRoute() {
+    	node1 = new AddressHolder("pl1.eng.monash.edu.au", DEFAULT_PORT);
+    	node2 = new AddressHolder("planetlab-4.eecs.cwru.edu", DEFAULT_PORT);
+    	node3 = new AddressHolder("planetlab1.cs.ubc.ca", DEFAULT_PORT);
+    	node4 = new AddressHolder("planetlab2.cs.ubc.ca", DEFAULT_PORT);
+    }
     
     @Override
     /**
@@ -34,6 +46,13 @@ public class DirectRoute implements RouteStrategy {
     	default: 
     		return null;
     	}
+    }
+    
+    public static synchronized DirectRoute getInstance() {
+        if (mDirectRoute == null) {
+        	mDirectRoute = new DirectRoute();
+        }
+        return mDirectRoute;
     }
     
     
