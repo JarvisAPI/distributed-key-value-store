7b8c18373440cf09fa269217c7e6ff98b4150574
William Ou <ouhaoqian@hotmail.com>
2019-03-04 10:18:20 -0800

    - Changed overload waittime

--- src/com/g8A/CPEN431/A8/protocol/Protocol.java
+++ src/com/g8A/CPEN431/A8/protocol/Protocol.java
@@ -1,7 +1,5 @@
 package com.g8A.CPEN431.A8.protocol;
 
-import java.util.concurrent.TimeUnit;
-
 public class Protocol {
     public static final int SIZE_MAX_KEY_LENGTH = 32;
     public static final int SIZE_MAX_VAL_LENGTH = 10000; // in bytes.
@@ -25,29 +23,8 @@ public class Protocol {
     public static final int ERR_INVALID_VAL = 0x07;
     
     private static final int INIT_OVERLOAD_WAITTIME = 100;
-    // Max overload waittime after which the overload waitime will not increase
-    // This does not mean this is the exact maximum
-    private static final int MAX_OVERLOAD_WAITTIME = 5000;
-    private static int overloadResponseCounter = 0;
-    private static final int OVERLOAD_RESPONSE_LIMIT = 5;
-    private static long lastOverloadedTime = Long.MIN_VALUE; // nano time when last overloaded response was sent
-    private static long overloadWaittime = INIT_OVERLOAD_WAITTIME; // Overload wait time to send in millis
-    
-    public static synchronized final int getOverloadWaittime() {
-        long curTime = System.nanoTime();
-        if (TimeUnit.NANOSECONDS.toMillis(curTime - lastOverloadedTime) < overloadWaittime) {
-            if (overloadWaittime < MAX_OVERLOAD_WAITTIME) {
-                if (++overloadResponseCounter > OVERLOAD_RESPONSE_LIMIT) {
-                    overloadResponseCounter = 0;
-                    overloadWaittime *= 2;
-                }
-            }
-        }
-        else {
-            overloadResponseCounter = 0;
-            overloadWaittime = INIT_OVERLOAD_WAITTIME;
-        }
-        lastOverloadedTime = curTime;
-        return (int) overloadWaittime;
+
+    public static final int getOverloadWaittime() {
+        return INIT_OVERLOAD_WAITTIME;
     }
 }
